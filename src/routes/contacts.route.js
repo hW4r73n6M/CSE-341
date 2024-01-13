d109f9f8c4a95501584513ca1f381abca95dd220
hW4r73n6M <gui20003@byui.edu>
2024-01-13 13:17:13 -0500

    create contacts route

---
+++ src/routes/contacts.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ctrlFn = require('../controllers/contacts.controllers');
+
+
+router.get('/all', ctrlFn.getAllProfessionals);
+router.get('/id/:id', ctrlFn.getProfessionalById);
+router.post('/create', ctrlFn.createNewProfessional);
+router.put('/update', ctrlFn.modifyCurrentProfessional);
+
+module.exports = router;
